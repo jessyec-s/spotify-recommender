61c9fc2ccd3127401087f1e2aa867fedb123f9fc
Jessye Coleman-Shapiro <cjessye@gmail.com>
2019-02-01 10:44:56 -0500

    Updates

---
+++ precache-manifest.07088d32c79d0205b0658f5579b33709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify-recommender/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d8a30f8e67d94b182c52",
+    "url": "/spotify-recommender/static/js/runtime~main.d8a30f8e.js"
+  },
+  {
+    "revision": "6e14a6691581a5579efe",
+    "url": "/spotify-recommender/static/js/main.6e14a669.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/spotify-recommender/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6e14a6691581a5579efe",
+    "url": "/spotify-recommender/static/css/main.e018abe2.chunk.css"
+  },
+  {
+    "revision": "198111e1381f16e9e9c0cf6bfa745e8a",
+    "url": "/spotify-recommender/index.html"
+  }
+];
